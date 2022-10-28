d0e0825e54acc5fbe608b556b9da8c0500a1a61a
robiat <zakariyahrobiat2018@gmail.com>
2022-10-28 17:54:58 +0000

    commit

--- script.js
+++ script.js
@@ -4,26 +4,38 @@ body = document.querySelector("body");
 bodyEl = document.querySelector(".body");
 button = document.querySelectorAll("button");
 navContainer = document.querySelector(".nav-container");
-contactContainer = document.querySelector(".contact-container");
+
 aboutContainer = document.querySelector(".about-container");
+project = document.querySelector(".project");
 const text = "Zakariyah Robiah";
 setInterval(writeName, 200);
 let index = 1;
 function writeName() {
   getName.innerText = text.slice(0, index);
   index++;
-  if (index > text.length) {
-    index = 1;
-  }
+  // if (index > text.length) {
+  //   index = 1;
+  // }
 }
 mode.addEventListener("change", () => {
   body.classList.toggle("active");
-  bodyEl.classList.toggle("active");
+  // bodyEl.classList.toggle("active");
   navContainer.classList.toggle("active");
-  contactContainer.classList.toggle("active");
+
   aboutContainer.classList.toggle("active");
   for (i = 0; i < button.length; i++) {
     button[i].classList.toggle("active");
   }
+  project.classList.toggle("active");
 });
-// writeName();
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const hidden = document.querySelectorAll(".hidden");
+hidden.forEach((el) => observer.observe(el));
