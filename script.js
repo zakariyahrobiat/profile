1672a35fa6e3ee18c81f0a2ad0be43e8ef3b656b
robiat <zakariyahrobiat2018@gmail.com>
2022-10-29 12:18:54 +0000

    commit

--- script.js
+++ script.js
@@ -39,3 +39,20 @@ const observer = new IntersectionObserver((entries) => {
 });
 const hidden = document.querySelectorAll(".hidden");
 hidden.forEach((el) => observer.observe(el));
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => alert("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
